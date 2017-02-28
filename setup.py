a108e0b69d1b4b254ac0c3c202bcf6707f63b91f
markuszm <markus_zimmermann-ffm@t-online.de>
2017-02-28 11:38:51 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='concourse_common',
+      version='0.1',
+      description='Common utitilies for concourse resources',
+      url='https://github.com/cosee-concourse/concourse-common.git',
+      author='cosee',
+      license='MIT',
+      packages=['concourse_common'],
+      zip_safe=False)
